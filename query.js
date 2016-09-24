59e0f08c7bae5e9fd65a7616c4d4a2a443382676
Ali <frank@gmail.com>
2016-09-24 19:47:10 +0500

    complete assignment

---
+++ query.js
@@ -0,0 +1,27 @@
+var num1 = prompt("Enter firsh number ", "1");
+var opt = prompt("Enter Operator" , "+");
+var num2 = prompt("Enter second number","1");
+num1  = Number(num1);
+num2 = Number(num2);
+var add;
+if(opt === "+"){
+    add = num1 + num2;
+	document.write(num1 + " + " +   num2 +  " = "  + add);	
+}
+else if(opt === "-"){
+    add = num1 - num2;	
+	document.write(num1 + " - " +   num2 +  " = "  + add);
+}
+else if(opt === "*"){
+    add = num1 * num2;	
+	document.write(num1 + " * " +   num2 +  " = "  + add);
+}
+else if(opt === "/"){
+    add = num1 / num2;	
+	document.write( num1 + " / " +   num2 +  " = "  + add);
+}
+else if(opt === "%"){
+    add = num1 % num2;	
+	document.write( num1 + " % " +   num2 +  " = "  + add);
+}
+
